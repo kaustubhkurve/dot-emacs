de6da4d2dadc25f909937dd39427bf013d57dc99
Kaustubh Kurve <kaustubh@localhost.localdomain>
2016-09-07 22:33:51 +0530

    Setup agenda files for org mode

--- init.el
+++ init.el
@@ -335,5 +335,6 @@
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
+(setq org-agenda-files (list "~/org/personal.org" "~/org/work.org"))
 
 ;; init.el ends here
