57aceb62cf2b4fbca0e11d330debc97442cd9eb7
Kaustubh Kurve <kaustubh.kurve@ignitesol.com>
2018-05-03 19:31:45 +0530

    Update init.el with changes from work

--- init.el
+++ init.el
@@ -36,6 +36,7 @@
     switch-window
     which-key
     js2-mode
+    xref-js2
     yaml-mode
     dockerfile-mode
     multiple-cursors
@@ -47,7 +48,13 @@
     intero
     toml-mode
     anti-zenburn-theme
+    scala-mode
     anzu
+    php-mode
+    ac-php
+    dumb-jump
+    tide
+    editorconfig
     ))
 
 (mapc #'(lambda (package)
@@ -55,17 +62,19 @@
       (package-install package)))
       myPackages)
 
-
 ;; BASIC CUSTOMIZATION
 ;; --------------------------------------
 
 (load-theme 'anti-zenburn t)
 (setq inhibit-startup-message t) ;; hide the startup message
 (column-number-mode 1) ;; show column/row in mode line
-;; (global-linum-mode t) ;; enable line numbers globally
 (delete-selection-mode 1) ;; Enable deletions when typing after a mark is active
 (setq custom-file "~/.emacs.d/local/custom-set.el")
 (load custom-file 'noerror)
+
+;; Save all backup files in /tmp
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
 
 ;; Company Mode
 ;; --------------------------------------
@@ -95,9 +104,6 @@
 
 (add-hook 'elpy-mode-hook #'smartparens-mode)
 
-;; enable autopep8 on save
-;; (require 'py-autopep8)
-;; (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
 
 ;; UI CONFIGURATION
 ;; --------------------------------------------------------------
@@ -107,14 +113,81 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(ansi-term-color-vector
+   [unspecified "#424242" "#EF9A9A" "#C5E1A5" "#FFEE58" "#64B5F6" "#E1BEE7" "#80DEEA" "#E0E0E0"])
+ '(background-color "#202020")
+ '(background-mode dark)
+ '(beacon-color "#ec4780")
+ '(cursor-color "#cccccc")
+ '(custom-safe-themes
+   (quote
+    ("086970da368bb95e42fd4ddac3149e84ce5f165e90dfc6ce6baceae30cf581ef" "0e0c37ee89f0213ce31205e9ae8bce1f93c9bcd81b1bcda0233061bb02c357a8" "15348febfa2266c4def59a08ef2846f6032c0797f001d7b9148f30ace0d08bcf" "4980e5ddaae985e4bae004280bd343721271ebb28f22b3e3b2427443e748cd3f" "9122dfb203945f6e84b0de66d11a97de6c9edf28b3b5db772472e4beccc6b3c5" "a2e7b508533d46b701ad3b055e7c708323fb110b6676a8be458a758dd8f24e27" "67e998c3c23fe24ed0fb92b9de75011b92f35d3e89344157ae0d544d50a63a72" "5cd0afd0ca01648e1fff95a7a7f8abec925bd654915153fb39ee8e72a8b56a1f" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "ff7625ad8aa2615eae96d6b4469fcc7d3d20b2e1ebc63b761a349bebbb9d23cb" "9be1d34d961a40d94ef94d0d08a364c3d27201f3c98c9d38e36f10588469ea57" "87d46d0ad89557c616d04bef34afd191234992c4eb955ff3c60c6aa3afc2e5cc" "6271fc9740379f8e2722f1510d481c1df1fcc43e48fa6641a5c19e954c21cc8f" "12670281275ea7c1b42d0a548a584e23b9c4e1d2dabb747fd5e2d692bcd0d39b" "ef04dd1e33f7cbd5aa3187981b18652b8d5ac9e680997b45dc5d00443e6a46e3" "16dd114a84d0aeccc5ad6fd64752a11ea2e841e3853234f19dc02a7b91f5d661" "dd4628d6c2d1f84ad7908c859797b24cc6239dfe7d71b3363ccdd2b88963f336" "aea30125ef2e48831f46695418677b9d676c3babf43959c8e978c0ad672a7329" "db2ecce0600e3a5453532a89fc19b139664b4a3e7cbefce3aaf42b6d9b1d6214" "3eb8dec7c15ce9eb3584ec9846175d963f838eaeccf2490f8ab3daff59862212" "8062d7fd259d3232d69b38db3b15d4ac44a70bf620cbc5b3926a6e16c74d6a5a" "23ccf46b0d05ae80ee0661b91a083427a6c61e7a260227d37e36833d862ccffc" "63dd8ce36f352b92dbf4f80e912ac68216c1d7cf6ae98195e287fd7c7f7cb189" "c3e6b52caa77cb09c049d3c973798bc64b5c43cc437d449eacf35b3e776bf85c" "70f5a47eb08fe7a4ccb88e2550d377ce085fedce81cf30c56e3077f95a2909f2" "5a0eee1070a4fc64268f008a4c7abfda32d912118e080e18c3c865ef864d1bea" "4e753673a37c71b07e3026be75dc6af3efbac5ce335f3707b7d6a110ecb636a3" default)))
+ '(elpy-syntax-check-command "pylint")
+ '(evil-emacs-state-cursor (quote ("#E57373" hbar)))
+ '(evil-insert-state-cursor (quote ("#E57373" bar)))
+ '(evil-normal-state-cursor (quote ("#FFEE58" box)))
+ '(evil-visual-state-cursor (quote ("#C5E1A5" box)))
+ '(fci-rule-color "#383838")
+ '(flycheck-color-mode-line-face-to-color (quote mode-line-buffer-id))
+ '(foreground-color "#cccccc")
+ '(highlight-indent-guides-auto-enabled nil)
+ '(highlight-symbol-colors
+   (quote
+    ("#FFEE58" "#C5E1A5" "#80DEEA" "#64B5F6" "#E1BEE7" "#FFCC80")))
+ '(highlight-symbol-foreground-color "#E0E0E0")
+ '(highlight-tail-colors (quote (("#ec4780" . 0) ("#424242" . 100))))
+ '(hl-paren-background-colors
+   (quote
+    ("#00FF99" "#CCFF99" "#FFCC99" "#FF9999" "#FF99CC" "#CC99FF" "#9999FF" "#99CCFF" "#99FFCC" "#7FFF00")))
+ '(hl-paren-colors
+   (quote
+    ("#B9F" "#B8D" "#B7B" "#B69" "#B57" "#B45" "#B33" "#B11")))
  '(magit-log-arguments (quote ("--graph" "--color" "--decorate" "-n256")))
  '(menu-bar-mode nil)
+ '(nrepl-message-colors
+   (quote
+    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
+ '(org-export-backends (quote (ascii html icalendar latex md)))
  '(package-selected-packages
    (quote
-    (intero haskell-mode yaml-mode which-key switch-window smartparens restclient racer multiple-cursors magit key-chord js2-mode helm-projectile helm-gtags helm-ag flycheck-rust flatui-theme expand-region evil-tutor elpy dockerfile-mode crux company-racer company-jedi company-go better-defaults aggressive-indent ag)))
+    (web-mode virtualenvwrapper noctilux-theme anti-zenburn-theme solarized-theme visual-regexp symbol-overlay tao-theme hungry-delete git-timemachine git-messenger json-mode markdown-mode intero haskell-mode yaml-mode which-key switch-window smartparens restclient racer multiple-cursors magit key-chord js2-mode helm-projectile helm-gtags helm-ag flycheck-rust flatui-theme expand-region evil-tutor elpy dockerfile-mode crux company-racer company-jedi company-go better-defaults aggressive-indent ag)))
+ '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
+ '(pos-tip-background-color "#3a3a3a")
+ '(pos-tip-foreground-color "#9E9E9E")
  '(send-mail-function (quote mailclient-send-it))
  '(show-paren-mode t)
- '(tool-bar-mode nil))
+ '(sml/active-background-color "#34495e")
+ '(sml/active-foreground-color "#ecf0f1")
+ '(sml/inactive-background-color "#dfe4ea")
+ '(sml/inactive-foreground-color "#34495e")
+ '(tabbar-background-color "#353535")
+ '(tool-bar-mode nil)
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#BC8383")
+     (40 . "#CC9393")
+     (60 . "#DFAF8F")
+     (80 . "#D0BF8F")
+     (100 . "#E0CF9F")
+     (120 . "#F0DFAF")
+     (140 . "#5F7F5F")
+     (160 . "#7F9F7F")
+     (180 . "#8FB28F")
+     (200 . "#9FC59F")
+     (220 . "#AFD8AF")
+     (240 . "#BFEBBF")
+     (260 . "#93E0E3")
+     (280 . "#6CA0A3")
+     (300 . "#7CB8BB")
+     (320 . "#8CD0D3")
+     (340 . "#94BFF3")
+     (360 . "#DC8CC3"))))
+ '(vc-annotate-very-old-color "#DC8CC3"))
 
 (setq initial-frame-alist '(
   (font . "Source Code Pro Medium-11")
@@ -142,12 +215,12 @@
   (add-to-list 'company-backends 'company-go))
 
 (require 'go-mode)
-;; (require 'go-mode-autoloads)
+(require 'go-mode-autoloads)
 
-(setenv "GOPATH" "/home/kaustubh/workspace/go-projects")
+(setenv "GOPATH" "/home/kaustubh/Workspace/GoProjects")
 
 (setq exec-path (cons "/usr/local/go/bin" exec-path))
-(add-to-list 'exec-path "/home/kaustubh/workspace/go-projects/bin")
+(add-to-list 'exec-path "$GOPATH/bin")
 
 (load-file "$GOPATH/src/golang.org/x/tools/cmd/oracle/oracle.el")
 
@@ -245,8 +318,8 @@
 
 (defun my-fullscreen ()
   (interactive)
-  (set-frame-parameter nil 'fullscreen 'fullboth) ;this makes the frame go fullscreen
-  (tool-bar-mode -1) ;these 3 lines turn off GUI junk
+  (set-frame-parameter nil 'fullscreen 'fullboth)
+  (tool-bar-mode -1)
   (scroll-bar-mode -1)
   (menu-bar-mode -1))
 
@@ -254,11 +327,11 @@
   (interactive)
   (set-frame-parameter nil 'width 82)
   (set-frame-parameter nil 'fullscreen 'fullheight)
-  (menu-bar-mode t)) ;I don't turn tool-bar and scroll-bar back on b/c I never want them
+  (menu-bar-mode t))
 
 (defun toggle-fullscreen ()
   (interactive)
-  (if (eq (frame-parameter nil 'fullscreen) 'fullboth)  ;tests if already fullscreened
+  (if (eq (frame-parameter nil 'fullscreen) 'fullboth)
       (my-non-fullscreen)
     (my-fullscreen)))
 
@@ -272,7 +345,6 @@
 (projectile-global-mode)
 (setq projectile-completion-system 'helm)
 (helm-projectile-on)
-
 (setq projectile-switch-project-action 'helm-projectile)
 
 ;; MAGIT CONFIGURATION
@@ -295,8 +367,15 @@
 ;; JAVASCRIPT CONFIGURATION
 ;; -------------------------------------------------------------------
 
-(add-hook 'js-mode-hook 'js2-minor-mode)
-(add-hook 'js2-mode-hook 'ac-js2-mode)
+(require 'xref-js2)
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
+;; (add-hook 'js-mode-hook 'js2-minor-mode)
+;; (add-hook 'js2-mode-hook 'ac-js2-mode)
+(define-key js-mode-map (kbd "M-.") nil)
+
+(add-hook 'js2-mode-hook (lambda ()
+  (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
 
 ;; RUST MODE CONFIGURATION
 (add-hook 'rust-mode-hook #'racer-mode)
@@ -351,7 +430,78 @@
 ;; TOML Configuration
 (require 'toml-mode)
 
-;; Anzu configuration
+;; Enable anzu mode
 (global-anzu-mode +1)
+
+;; Enable PHP mode
+(require 'php-mode)
+
+;; Highlight Comment Annotations
+(defun font-lock-comment-annotations ()
+  (font-lock-add-keywords
+   nil '(("\\<\\(FIX\\(ME\\)?\\|TODO\\|HACK\\|REFACTOR\\):"
+          1 font-lock-warning-face t))))
+
+(add-hook 'prog-mode-hook 'font-lock-comment-annotations)
+
+;; bind json pretty print. This is a global keybinding. Some minor modes
+;; may overwrite this
+(global-set-key (kbd "C-x j") 'json-pretty-print)
+
+;; Dired mode customizations
+(add-hook 'dired-mode-hook
+	  (lambda ()
+	    (dired-omit-mode 1)
+	    (setq dired-omit-files
+		  (concat dired-omit-files "\\|\\.pyc$"))
+	    ))
+
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.ctp?\\'" . web-mode))
+
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 4)
+  (setq web-mode-css-indent-offset 4)
+  (setq web-mode-code-indent-offset 4)
+  (setq web-mode-indent-style 4)
+  )
+
+(add-hook 'web-mode-hook  'my-web-mode-hook)
+
+;; Dump Jump Configuration
+(dumb-jump-mode)
+
+;; TIDE CONFIGURATION
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
+
+;; formats the buffer before saving
+(add-hook 'before-save-hook 'tide-format-before-save)
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
+
+;; EditorConfig Configuration
+(require 'editorconfig)
+(editorconfig-mode 1)
 
 ;; init.el ends here
